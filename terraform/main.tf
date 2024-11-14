21c9905562b07aed7872393fc434cf759d0bbede
AshrafArena <achrafreda98@outlook.com>
2024-11-14 15:06:13 +0000

    added tf modules

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source     = "./modules/vpc"
+  cidr_block = var.vpc_cidr
+}
+
+module "security_groups" {
+  source = "./modules/security-groups"
+  vpc_id = module.vpc.vpc_id
+}
+
+module "ecr" {
+  source    = "./modules/ecr"
+  repo_name = var.ecr_repo_name
+}
+
+module "ecs" {
+  source            = "./modules/ecs"
+  cluster_name      = var.cluster_name
+  app_name          = var.app_name
+  ecr_image         = module.ecr.repository_url
+  security_group_id = module.security_groups.ecs_sg_id
+  subnets           = module.vpc.public_subnets
+}
+
+module "alb" {
+  source            = "./modules/alb"
+  vpc_id            = module.vpc.vpc_id
+  subnets           = module.vpc.public_subnets
+  security_group_id = module.security_groups.alb_sg_id
+  target_group_arn  = module.ecs.target_group_arn
+  certificate_arn   = var.acm_certificate_arn
+}
+
+module "acm" {
+  source      = "./modules/acm"
+  domain_name = "${var.subdomain}.${var.domain_name}"
+}
+
+module "route53" {
+  source        = "./modules/route53"
+  domain_name   = var.domain_name
+  subdomain     = var.subdomain
+  alb_dns_name  = module.alb.alb_dns_name
+}
