daa322ba3a8bf4f73716a925be3cb66a97946ab0
AshrafArena <achrafreda98@outlook.com>
2024-11-15 18:35:33 +0000

    updated tf structure

--- terraform/modules/ecs/variables.tf
+++ terraform/modules/ecs/variables.tf
@@ -18,10 +18,6 @@ variable "security_group_id" {
   description = "Security group ID for ECS tasks."
 }
 
-variable "execution_role_arn" {
-  description = "The ARN of the execution role for the ECS task."
-}
-
 variable "target_group_arn" {
   description = "The target group ARN for the ALB."
 }
