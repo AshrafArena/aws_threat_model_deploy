fa4b704edc465561da6a5a38d88406af639ec1a9
AshrafArena <achrafreda98@outlook.com>
2024-11-16 15:29:26 +0000

    updated tf structure

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -12,6 +12,11 @@ resource "aws_lb_target_group" "app" {
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
   target_type = "ip"
+
+  health_check {
+  path     = "/"
+  protocol = "HTTP"
+}
 }
 
 resource "aws_lb_listener" "http" {
@@ -19,12 +24,15 @@ resource "aws_lb_listener" "http" {
   port              = 80
   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
-  }
+    default_action {
+    type = "redirect"
+    redirect {
+      protocol    = "HTTPS"
+      port        = "443"
+      status_code = "HTTP_301"
+    }
 }
-
+}
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.this.arn
   port              = 443
