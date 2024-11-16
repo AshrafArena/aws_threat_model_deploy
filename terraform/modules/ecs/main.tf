97d53f9f90d42a4658da736b2139b14ae134b333
AshrafArena <achrafreda98@outlook.com>
2024-11-16 01:18:17 +0000

    updated tf structure

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -21,6 +21,7 @@ resource "aws_ecs_task_definition" "app" {
 }
 
 resource "aws_ecs_service" "app" {
+  name            = "${var.app_name}-service"
   cluster        = aws_ecs_cluster.this.id
   task_definition = aws_ecs_task_definition.app.arn
   desired_count   = 1
